be925db84fac010177abe2b9549023c1f66e8f8c
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-14 22:38:08 +0000

    basic stateful widget

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,26 @@
 library login_screen;
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  LoginScreenState createState() {
+    return new LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
 }
