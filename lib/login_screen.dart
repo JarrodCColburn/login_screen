e075b361814235c94337a128dc5cfa9f127aa28d
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-15 00:01:12 +0000

    login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,19 +1,3 @@
 library login_screen;
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'src/login_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  LoginScreenState createState() {
-    return new LoginScreenState();
-  }
-}
-
-class LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
-  }
-}
