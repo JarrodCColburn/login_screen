ee6c475a2b6d1e98bc30a31aa03921226995ed8b
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-15 23:17:10 +0000

    pages are tabs

--- lib/src/auth.dart
+++ lib/src/auth.dart
@@ -3,14 +3,38 @@ import './widget/login_screen.dart';
 import './util/auth.dart';
 
 class AuthPage extends StatelessWidget {
+  AuthPage({this.logo});
+
+  final Widget logo;
+
+  List<Widget> get views => [
+    Container(child: Center(child: Text('hello'),),),
+        LoginScreen(
+          credentialsCallback: Auth.instance.emailSignIn,
+          logo: logo,
+        ),
+    Container(child: Center(child: Text('hello'),),)
+
+      ];
+
+  List<Tab> get tabs => [
+   Tab( text: "Retrieve",),
+   Tab(text: "Login",),
+   Tab( text: "Sign Up",)
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      body: SafeArea(
-          child: LoginScreen(
-        credentialsCallback: Auth.instance.emailSignIn,
-      )),
+    return DefaultTabController(
+      length: views.length,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(bottom: TabBar(tabs: tabs)),
+        body: SafeArea(
+          child: TabBarView(children: views),
+        ),
+      ),
     );
   }
 }
