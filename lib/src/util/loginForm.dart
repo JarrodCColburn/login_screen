e1219b38e5a9a003aeecbcc8658dd5cce861da4c
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-15 07:43:51 +0000

    added form field validator for blank eamil/passowrd

--- lib/src/util/loginForm.dart
+++ lib/src/util/loginForm.dart
@@ -5,8 +5,16 @@ class LoginForm {
   final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
   final TextEditingController email = new TextEditingController();
   final TextEditingController password = new TextEditingController();
-  final FormTextValidator emailValidator;
-  final FormTextValidator passwordValidator;
+  final FormTextValidator emailValidator = (String value){
+    if(value.trim().isEmpty){
+      return 'Cannot be empty';
+    }
+  };
+  final FormTextValidator passwordValidator = (String value){
+    if(value.trim().isEmpty){
+      return 'Cannot be empty';
+    }
+  };
 
-  LoginForm({this.emailValidator, this.passwordValidator});
+  LoginForm();
 }
