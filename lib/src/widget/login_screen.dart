58a401c330dd1d252f75eb0afaf1a71c90b5352e
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-15 09:31:01 +0000

    add image, resize on keyboard

--- lib/src/widget/login_screen.dart
+++ lib/src/widget/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import '../util/loginForm.dart';
 
 typedef CredentialsCallback = Future<void> Function(
@@ -29,15 +30,18 @@ class LoginScreen extends StatefulWidget {
 
 class LoginScreenState extends State<LoginScreen> {
   bool _isPasswordVisible = true;
-  void toggleVisibility(){
+
+  void toggleVisibility() {
     setState(() {
-     _isPasswordVisible =! _isPasswordVisible;
+      _isPasswordVisible = !_isPasswordVisible;
     });
   }
 
   bool _isLoading = false;
-  void startLoading()=> setState(() => _isLoading = true);
-  void endLoading()=> setState(() => _isLoading = false);
+
+  void startLoading() => setState(() => _isLoading = true);
+
+  void endLoading() => setState(() => _isLoading = false);
 
   void _handleSubmit() {
     bool isValid = widget.loginForm.formKey.currentState.validate();
@@ -49,34 +53,38 @@ class LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  Widget _buildForm() => Form(
+  Widget _buildForm(BuildContext context) => Form(
         key: widget.loginForm.formKey,
         child: Column(
           children: <Widget>[
-            _buildEmailField(),
-            _buildPasswordField(),
-            _buildSubmitButton(),
+            _buildEmailField(context),
+            _buildPasswordField(context),
+            _buildSubmitButton(context),
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         ),
       );
 
-  Widget _buildSubmitButton() => RaisedButton(
+  Widget _buildSubmitButton(BuildContext context) => RaisedButton(
         onPressed: _handleSubmit,
         child: (_isLoading) ? CircularProgressIndicator() : Text('Submit'),
+        shape: RoundedRectangleBorder(),
       );
 
-  Widget _buildEmailField() => TextFormField(
+  Widget _buildEmailField(BuildContext context) => TextFormField(
         controller: widget.loginForm.email,
         validator: widget.loginForm.emailValidator,
         obscureText: false,
         decoration: InputDecoration(
           labelText: 'Email',
-          icon: Icon(Icons.email),
+          icon: Icon(
+            Icons.email,
+            color: Theme.of(context).accentColor,
+          ),
         ),
       );
 
-  Widget _buildPasswordField() => TextFormField(
+  Widget _buildPasswordField(BuildContext context) => TextFormField(
         controller: widget.loginForm.password,
         validator: widget.loginForm.passwordValidator,
         obscureText: _isPasswordVisible,
@@ -85,14 +93,40 @@ class LoginScreenState extends State<LoginScreen> {
           icon: GestureDetector(
             onTap: toggleVisibility,
             child: (_isPasswordVisible)
-                ? Icon(Icons.visibility)
-                : Icon(Icons.visibility_off),
+                ? Icon(
+                    Icons.visibility,
+                    color: Theme.of(context).accentColor,
+                  )
+                : Icon(
+                    Icons.visibility_off,
+                    color: Theme.of(context).accentColor,
+                  ),
           ),
         ),
       );
 
+  bool isKeyboard(BuildContext context) =>
+      MediaQuery.of(context).viewInsets.bottom <= 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: _buildForm()));
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildForm(context),
+          (isKeyboard(context))
+              ? Container(
+                  child: Image.network(
+                    'https://cdn.pixabay.com/photo/2012/04/01/19/29/tower-24208_960_720.png',
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                )
+              : Container()
+        ],
+      )),
+    );
   }
 }
