8ce3b85f74fabd4840d53570d94e9526ce8d4cd6
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-15 04:03:31 +0000

    include rx dart

---
+++ lib/src/widget/login_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+/// Return a string if an error (ex: 'no user'), returns null if no success.
+typedef CredentialsCallback = Future<String> Function(
+    String email, String password);
+typedef FormTextValidator = String Function(String value);
+
+class LoginForm {
+  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+  final TextEditingController email = new TextEditingController();
+  final TextEditingController password = new TextEditingController();
+  final FormTextValidator emailValidator;
+  final FormTextValidator passwordValidator;
+
+  LoginForm({this.emailValidator, this.passwordValidator});
+}
+
+String emailValidator(String email) {
+  // TODO Validate, will return string if not valid
+  return null;
+}
+
+String passwordValidator(String password) {
+  // TODO Validate, will return string if not valid
+  return null;
+}
+
+class LoginScreen extends StatefulWidget {
+  final CredentialsCallback credentialsCallback;
+
+  LoginScreen({this.credentialsCallback});
+
+  final LoginForm loginForm = new LoginForm(
+      emailValidator: emailValidator, passwordValidator: passwordValidator);
+
+  @override
+  LoginScreenState createState() {
+    return new LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  bool isPasswordObscure = true;
+
+  void _handleSubmit() {
+    String email = widget.loginForm.email.value.text;
+    String password = widget.loginForm.password.value.text;
+    widget.credentialsCallback(email, password);
+  }
+
+  Widget _buildForm() => Form(
+        key: widget.loginForm.formKey,
+        child: Column(
+          children: <Widget>[
+            _buildEmailField(),
+            _buildPasswordField(),
+            _buildSubmitButton(),
+          ],
+        ),
+      );
+
+  Widget _buildSubmitButton() => RaisedButton(onPressed: _handleSubmit);
+
+  Widget _buildEmailField() => TextFormField(
+        controller: widget.loginForm.email,
+        obscureText: false,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          icon: Icon(Icons.email),
+        ),
+      );
+
+  Widget _buildPasswordField() => TextFormField(
+        controller: widget.loginForm.password,
+        obscureText: isPasswordObscure,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          icon: GestureDetector(
+            onTap: () => setState(() => isPasswordObscure = !isPasswordObscure),
+            child: (isPasswordObscure)
+                ? Icon(Icons.visibility)
+                : Icon(Icons.visibility_off),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildForm();
+  }
+}
